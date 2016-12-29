e7ac329e58c1a3037386604001e332ea877a82bd
Jaap Mengers <jaapm@q42.nl>
2016-12-29 17:22:32 +0100

    Strips off things like [...] - 2000 Digital Remaster from song title's to increase likelihood of finding matching chords

--- src/search.js
+++ src/search.js
@@ -3,7 +3,10 @@ const osmosis = require('osmosis');
 function getChordsForArtistAndSong(artist, song) {
   const results = [];
   return new Promise((resolve, reject) => {
-    const url = `https://www.ultimate-guitar.com/search.php?view_state=advanced&band_name=${artist}&song_name=${song}&type%5B%5D=300&version_la=`;
+    // Strip things like [...] - 2000 digital remaster off. We take the false positives for granted
+    const url = `https://www.ultimate-guitar.com/search.php?view_state=advanced&band_name=${artist}&song_name=${strip(song)}&type%5B%5D=300&version_la=`;
+
+    console.log('URL', url);
 
     osmosis
     .get(url)
@@ -20,6 +23,17 @@ function getChordsForArtistAndSong(artist, song) {
     .done(() => resolve(results))
     .error(err => reject(err));
   });
+}
+
+function strip(str) {
+  const regex = /^([^(|^-]*)/g;
+  const matches = str.match(regex);
+
+  if (matches.length > 0) {
+    return matches[0].trim();
+  }
+
+  return '';
 }
 
 function getTopChordsPage(results) {
